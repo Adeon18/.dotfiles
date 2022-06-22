8e4512c39b34e2639cba63591dfa651dc0afdb33
Adeon18 <ostaptrush18@gmail.com>
2022-06-22 15:04:22 +0300

    Added a beta version of a proper script

--- install.sh
+++ install.sh
@@ -1,64 +1,139 @@
-#!/bin/sh
+#!/bin/bash
+#
+#
+readonly DEP_FILE="dependencies.txt"
+readonly SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-zsh=$1
-kitty=$2
-kitty_theme=$3
-ulauncher=$4
-neofetch=$5
-htop=$6
+ignore_uninstalled=false
+install_if_uninstalled=false
+default=false
+installed_dependancies=()
 
-if $zsh
-then
-	echo 'Installing zsh config'
-	rm $HOME/.zshrc
-	ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+# Checks if a program is installed
+# Arguments: program_name
+# Returns: 0 if exists, 1 if does not
+check_for_existance() {
+  if (( $# == 0 )); then
+    echo "ERROR: check_for_existance function needs a program name to run"
+  fi
+
+  if [[ $(command -v "$1") ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Manages the config files of all valid dependancies
+# If links exists, relinks them if they are older than configs(useless, but let it be).
+# If it does not, removes current config and links the needed one.
+# If there is no config, creates everything needed for one.
+# Arguments:
+#   $1 a directory for the config
+#   $2 config file name
+# Returns:
+#   none
+manage_configs() {
+  if (( $# < 2 )); then
+    echo "ERROR: Not enough arguments for manage_configs call"
+  fi
+
+  echo -n "Setting configs for $1..."
+  sleep 0.1
+  local conf_path
+  conf_path="${HOME}/.config/$1/$2"
+
+  if ! [[ -d "${HOME}/.config/$1" ]]; then
+    mkdir -p "${HOME}/.config/$1"
+  fi
+  if [[ -f "$conf_path" ]]; then
+    if [[ -L "$conf_path" ]]; then
+      # if [[ "${SCRIPT_DIR}/$1/$2" -nt "$conf_path" ]]; then
+        rm "$conf_path"
+        ln -s "${SCRIPT_DIR}/$1/$2" "$conf_path"
+      # fi
+    else
+      rm "$conf_path"
+      ln -s "${SCRIPT_DIR}/$1/$2" "$conf_path"
+    fi
+  else
+    ln -s "${SCRIPT_DIR}/$1/$2" "$conf_path"
+  fi
+
+  echo "Done"
+}
+
+# Manage script options
+if (( $# != 0 )); then
+  case $1 in
+    -i | --ignore-uninstalled)
+      ignore_uninstalled=true
+      ;;
+    -I | --install-uninstalled)
+      install_if_uninstalled=true
+      ;;
+    -h | --help)
+      echo "Usage: ./install.sh [option]
+    Options:
+      -h      --help                  Show help message.
+      -i      --ignore-uninstalled    Configure only installed dependancies.
+      -h      --install-uninstalled   Install dependancies if uninstalled and configure all.
+      -d      --default               Default installation, halts if sees an unisntalled dependancy."
+      exit 0
+      ;;
+    -d | --default)
+      default=true
+      ;;
+    *)
+      echo "Invalid option: $1. Use ./install.sh -h to see available options."
+      exit 0
+      ;;
+  esac
 else
-	echo 'Skipping zsh config'
+  echo "Please provide an option. To view all options run ./install.sh -h"
+  exit 0
 fi
 
-if $kitty
-then
-	echo 'Installing kitty config'
-	rm $HOME/.config/kitty/kitty.conf
-	ln -s $HOME/.dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
-else
-	echo 'Skipping kitty config'
-fi
+# Manage dependancies
+while read dependancy
+do
+  check_for_existance ${dependancy}
+  if (( $? != 0 )); then
+    if [[ "$default" == true ]]; then
+      echo "Dependency ${dependancy} is NOT installed. Please install it and run the script again."
+      exit 0
+    elif [[ "$ignore_uninstalled" == true ]]; then
+      echo "Ignoring ${dependancy} as it is NOT installed."
+    elif [[ "$install_if_uninstalled" == true ]]; then
+      echo "Here goes the code that is no here yet"
+    fi
+  else
+    installed_dependancies+=(${dependancy})
+    echo "Dependency ${dependancy} is installed"
+  fi
+  sleep 0.1
+done < ${DEP_FILE}
 
-if $kitty_theme
-then
-	echo 'Installing kitty config'
-	ln -s $HOME/.dotfiles/kitty/current-theme.conf $HOME/.config/kitty/current-theme.conf
-else
-	echo 'Skipping kitty-theme config'
-fi
+echo "-----------"
+# Connecting all the configs
+for dep in "${installed_dependancies[@]}"
+do
+  if [[ "$dep" == "bspwm" ]] || [[ "$dep" == "sxhkd" ]] || [[ "$dep" == "dunst" ]]; then
+    manage_configs "$dep" "${dep}rc"
+  elif [[ "$dep" == "kitty" ]]; then
+    manage_configs "$dep" "${dep}.conf"
+    manage_configs "$dep" "current-theme.conf"
+  elif [[ "$dep" == "rofi" ]]; then
+    manage_configs "$dep" "config.rasi"
+  elif [[ "$dep" == "polybar" ]]; then
+    manage_configs "$dep" "config"
+  elif [[ "$dep" == "neofetch" ]]; then
+    manage_configs "$dep" "config.conf"
+  fi
+  sleep 0.2
+done
 
-if $ulauncher
-then
-	echo 'Installing ulauncher config'
-	rm $HOME/.config/ulauncher/settings.json
-	rm $HOME/.config/ulauncher/shortcuts.json
-	ln -s $HOME/.dotfiles/ulauncher/settings.json $HOME/.config/ulauncher/settings.json
-	ln -s $HOME/.dotfiles/ulauncher/shortcuts.json $HOME/.config/ulauncher/shortcuts.json
-else
-	echo 'Skipping ulauncher config'
-fi
 
-if $neofetch
-then
-	echo 'Installing neofetch config'
-	rm $HOME/.config/neofetch/config.conf
-	ln -s $HOME/.dotfiles/neofetch/config.conf $HOME/.config/neofetch/config.conf
-else
-	echo 'Skipping neofetch config'
+if ! [[ -f "${HOME}/.xinitrc" ]]; then
+  echo "exec bspwm" > ~/.xinitrc
 fi
-
-if $htop
-then
-	echo 'Installing htop config'
-	rm $HOME/.config/htop/htoprc
-	ln -s $HOME/.dotfiles/htop/.config/htop/htoprc $HOME/.config/htop/htoprc
-else
-	echo 'Skipping htop config'
-fi
-
